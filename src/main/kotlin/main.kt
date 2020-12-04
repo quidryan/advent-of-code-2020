ffe607e696750f2c9626b70c2ba12c95529aeaa5
Justin Ryan <jryan@netflix.com>
2020-12-03 22:24:04 -0800

    Day 4

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,6 +7,8 @@ fun main(args: Array<String>) {
     day2_2()
     day3_1()
     day3_2()
+    day4_1()
+    day4_2()
 }
 
 fun getResourceAsText(path: String): InputStream {
@@ -113,4 +115,135 @@ fun day3_2() {
     val trees = paths.reduce { acc, elem -> acc * elem }
 
     println("Day 3_2: Number of trees: $trees")
+}
+
+data class Passport(
+    val byr: String?, // (Birth Year)
+    val iyr: String?, //  (Issue Year)
+    val eyr: String?, //  (Expiration Year)
+    val hgt: String?, //  (Height)
+    val hcl: String?, //  (Hair Color)
+    val ecl: String?, //  (Eye Color)
+    val pid: String?, //  (Passport ID)
+    val cid: String?, //  (Country ID))
+) {
+
+    private val requiredFields = listOf(byr, iyr, eyr, hgt, hcl, ecl, pid)
+
+    fun isValid() : Boolean {
+        return !requiredFields.any { it == null }
+    }
+}
+
+class Progress(val passports: List<Passport> = listOf(), val buf: String = "")
+
+fun createPassport(buf:String): Passport {
+    val fieldMap = buf.trim().split(" ").associate {
+        val (key, value) = it.split(":")
+        key to value
+    }
+    return Passport(
+        fieldMap["byr"],
+        fieldMap["iyr"],
+        fieldMap["eyr"],
+        fieldMap["hgt"],
+        fieldMap["hcl"],
+        fieldMap["ecl"],
+        fieldMap["pid"],
+        fieldMap["cid"]
+    )
+}
+
+fun createValidPassport(buf:String): Passport {
+    val fieldMap = buf.trim().split(" ").associate {
+        val (key, value) = it.split(":")
+        key to value
+    }
+    return Passport(
+        if (validYear(1920, 2002, fieldMap["byr"])) fieldMap["byr"] else null,
+        if (validYear(2010, 2020, fieldMap["iyr"])) fieldMap["iyr"] else null,
+        if (validYear(2020, 2030, fieldMap["eyr"])) fieldMap["eyr"] else null,
+        if (validHeight(fieldMap["hgt"])) fieldMap["hgt"] else null,
+        if (validHairColor(fieldMap["hcl"])) fieldMap["hcl"] else null,
+        if (validEyeColor(fieldMap["ecl"])) fieldMap["ecl"] else null,
+        if (validID(fieldMap["pid"])) fieldMap["pid"] else null,
+        fieldMap["cid"]
+    )
+}
+
+fun validYear(min: Int, max:Int, value: String?) : Boolean {
+    if (value.isNullOrEmpty()) return false
+    val year = value.toInt()
+    return (year in min..max)
+}
+
+fun validHeight(value:String?) : Boolean {
+    if (value == null) return false
+    val unit = value.takeLast(2)
+    val num = value.dropLast(2).toIntOrNull() ?: return false
+    return when (unit) {
+        "cm" -> {
+            num in 150..193
+        }
+        "in" -> {
+            num in 59..76
+        }
+        else -> {
+            false
+        }
+    }
+}
+
+fun validHairColor(value: String?) : Boolean {
+    if( value == null) return false
+    return value.matches(Regex("\\#[0-9a-f]{6}"))
+}
+
+fun validEyeColor(value: String?) : Boolean {
+    return "amb blu brn gry grn hzl oth".split(" ").contains(value)
+}
+
+fun validID(value: String?) : Boolean {
+    if( value == null) return false
+    return value.matches(Regex("[0-9]{9}"))
+}
+
+fun day4_1() {
+    val initialProgress = Progress()
+    val progress = getResourceAsText("day4_input.txt").bufferedReader().readLines()
+        .fold(initialProgress, { acc, line ->
+            if (line != "") {
+                Progress(acc.passports, acc.buf + " " + line)
+            } else {
+                // We have a passport, so generate it and add it too the progress
+                val passports = acc.passports + createPassport(acc.buf)
+                Progress(passports, "")
+            }
+        })
+    // The left over bits, make up the last passport
+    val passports = progress.passports + createPassport(progress.buf)
+
+    val valid = passports.filter { it.isValid() }
+
+    println("Valid passports: ${valid.size}")
+}
+
+fun day4_2() {
+    val initialProgress = Progress()
+    val progress = getResourceAsText("day4_input.txt").bufferedReader().readLines()
+        .fold(initialProgress, { acc, line ->
+            if (line != "") {
+                Progress(acc.passports, acc.buf + " " + line)
+            } else {
+                // We have a passport, so generate it and add it too the progress
+                val passports = acc.passports + createValidPassport(acc.buf)
+                Progress(passports, "")
+            }
+        })
+    // The left over bits, make up the last passport
+    val passports = progress.passports + createValidPassport(progress.buf)
+
+    val valid = passports.filter { it.isValid() }
+
+    println("Valid passports part 2: ${valid.size}")
 }
