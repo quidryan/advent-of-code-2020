cfff73e11bb21b6fb421a32f9386554171b5a7a1
Justin Ryan <jryan@netflix.com>
2020-12-14 22:22:14 -0800

    Day 15

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,6 +1,6 @@
 
 fun main(args: Array<String>) {
-    day15_1()
+    //day15_1()
     day15_2()
 }
 
@@ -19,16 +19,16 @@ fun day15_1() {
 }
 
 fun day15_2() {
-    playGameAndAssert("0,3,6", "175594", 30000000)
-    playGameAndAssert("1,3,2", "2578", 30000000)
-    playGameAndAssert("2,1,3", "3544142", 30000000)
-    playGameAndAssert("1,2,3", "261214", 30000000)
-    playGameAndAssert("2,3,1", "6895259", 30000000)
-    playGameAndAssert("3,2,1", "18", 30000000)
-    playGameAndAssert("3,1,2", "362", 30000000)
+    playGameAndAssert("0,3,6", "175594", 30_000_000)
+    playGameAndAssert("1,3,2", "2578", 30_000_000)
+    playGameAndAssert("2,1,3", "3544142", 30_000_000)
+    playGameAndAssert("1,2,3", "261214", 30_000_000)
+    playGameAndAssert("2,3,1", "6895259", 30_000_000)
+    playGameAndAssert("3,2,1", "18", 30_000_000)
+    playGameAndAssert("3,1,2", "362", 30_000_000)
 
     val input = "12,1,16,3,11,0"
-    val answer = playGame(input, 30000000)
+    val answer = playGame(input, 30_000_000)
     println("Answer at 2020 is $answer")
 }
 
@@ -43,26 +43,32 @@ fun playGameAndAssert(input: String, expectStr: String, whenToEnd: Int = 2020) {
 private fun playGame(input: String, whenToEnd: Int): Int {
     val initial = input.split(',').map { it.toInt() }.toMutableList()
 
+    val lastSeen = mutableMapOf<Int, Pair<Int, Int>>()
     initial.forEachIndexed { idx, value ->
-        //println("Turn $idx: ${value}")
-    }
-    // turn is the game version of turn, not the index in list
-    for (turn in initial.size+1..whenToEnd) {
-        val previousTurn = turn - 1
-        val recentIndex = previousTurn - 1
-        val recent = initial[recentIndex]
-        val hasBeenSpoken = initial.filter { it == recent }
-        if (hasBeenSpoken.size == 1) { // Only spoken once
-            initial.add(0)
-        } else {
-            val previouslySpoken = initial.slice(0 until recentIndex).lastIndexOf(recent) + 1 // convert to game turns.
-            initial.add(previousTurn - previouslySpoken)
-        }
-        //println("Turn $turn: ${initial.last()}")
+        lastSeen[value] = Pair(idx, -1)
+        //println("Turn ${idx+1}: $value")
     }
 
-    val last = initial.last()
-    println("Last element is $last")
-    return last
+    // turn is the game version of turn, not the index in list
+    var lastValue = initial.last()
+    for (turn in initial.size until whenToEnd) {
+        // Look up the last two times it's been seen
+        val previouslySpoken = lastSeen.getOrDefault(lastValue, Pair(-1, -1))
+
+        // The first reflects the what we just set.
+        // The second reflects the previous time we set it
+        lastValue = if (previouslySpoken.second == -1) { // Only spoken once
+            0
+        } else {
+            (turn - 1) - previouslySpoken.second
+        }
+
+        val updatePreviousVisits = lastSeen.getOrDefault(lastValue, Pair(-1, -1))
+        lastSeen[lastValue] = Pair(turn, updatePreviousVisits.first) // Move indexes over
+        //println("Turn ${turn+1}: $lastValue")
+    }
+
+    println("Last element is $lastValue")
+    return lastValue
 }
 
